19fddfba6249578689552592238c0a9a690e8fbd
Robin <froligr-57@outlook.fr>
2021-03-24 13:58:31 +0100

    updated InterfaceMorpion

--- Projet_serveur_journet_froliger_ravelli/src/modele/InterfaceMorpion.java
+++ Projet_serveur_journet_froliger_ravelli/src/modele/InterfaceMorpion.java
@@ -1,4 +1,13 @@
 package modele;
 
-public interface InterfaceMorpion {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface InterfaceMorpion  extends Remote{
+	public void initGrille() throws RemoteException;
+	public void PlacerX(int c) throws RemoteException;
+	public void PlacerO(int c) throws RemoteException;
+	public int testRanc(int a, int b, int c) throws RemoteException;
+	public int testGagnant() throws RemoteException;
+	public void AfficheGrille()throws RemoteException;
 }
